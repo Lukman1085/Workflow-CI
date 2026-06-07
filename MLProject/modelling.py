502bb9a60060249f479cb137545528065df65ab9
Andi Muh. Lukman Hakim <143136463+Lukman1085@users.noreply.github.com>
2026-06-07 10:17:07 +0800

    fix: force python 3.12.7 in mlflow conda_env to resolve docker pip build error

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -15,14 +15,27 @@ def train_and_save_model():
     X_train = train_data.drop(columns=['Burnout_Risk_Level'])
     y_train = train_data['Burnout_Risk_Level']
 
+    custom_env = {
+        "name": "burnout-env",
+        "channels": ["conda-forge"],
+        "dependencies": [
+            "python=3.12.7",  # Memaksa Docker menggunakan Python 3.12.7
+            "pip",
+            {"pip": ["mlflow==2.19.0", "scikit-learn", "pandas", "numpy"]},
+        ],
+    }
+
     with mlflow.start_run():
         # Menggunakan parameter terbaik dari hasil tuning sebelumnya
         model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
         model.fit(X_train, y_train)
 
-        # Menyimpan model ke direktori lokal agar bisa di-build oleh Docker
-        mlflow.sklearn.save_model(model, "model_dir")
-        print("Model berhasil dilatih dan disimpan ke direktori 'model_dir'")
+        mlflow.sklearn.save_model(
+            sk_model=model, 
+            path="model_dir", 
+            conda_env=custom_env
+        )
+        print("✅ Model CI berhasil dilatih dan disimpan dengan env Python 3.12.7")
 
 if __name__ == "__main__":
     train_and_save_model()
